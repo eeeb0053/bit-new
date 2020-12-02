ec96ef50a0522741924fde930962d3e0bd6f8604
eeeb0053 <eeeb0053@gmail.com>
2020-12-02 14:13:43 +0900

    20/12/02

---
+++ src/main/java/com/example/demo/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.repositories;
+
+import com.example.demo.domains.UserDTO;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository {
+    public int insert(UserDTO user);
+	public UserDTO login(UserDTO user);
+}
